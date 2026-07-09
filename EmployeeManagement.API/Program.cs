2ebcf11e9503e9fa8da46e622d9c975e33401f74
vishal-07-prajapati <vishalpprajaparti1234@outlook.com>
2026-07-09 12:38:44 +0530

    Configure SQL Server and register DbContext

--- EmployeeManagement.API/Program.cs
+++ EmployeeManagement.API/Program.cs
@@ -1,3 +1,6 @@
+using EmployeeManagement.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -17,7 +23,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+    
 app.MapControllers();
 
 app.Run();
