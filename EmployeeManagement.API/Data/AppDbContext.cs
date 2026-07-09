084b95c87e19eaeb7662d71c9d460a23fdc8d350
vishal-07-prajapati <vishalpprajaparti1234@outlook.com>
2026-07-09 12:02:18 +0530

    Add Entity Framework DbContext

---
+++ EmployeeManagement.API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagement.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
