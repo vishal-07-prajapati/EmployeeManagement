cea2609b6e9f91803c85f455b1c91667572ca0b3
vishal-07-prajapati <vishalpprajaparti1234@outlook.com>
2026-07-09 13:58:27 +0530

    Add employee CRUD controller

---
+++ EmployeeManagement.API/Controllers/EmployeeController.cs
@@ -0,0 +1,81 @@
+﻿using EmployeeManagement.API.Interfaces;
+using EmployeeManagement.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        #region DI
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+        #endregion
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var employees = await _employeeService.GetAllEmployeesAsync();
+
+            return Ok(employees);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var employee = await _employeeService.GetEmployeeByIdAsync(id);
+
+            if (employee == null)
+                return NotFound();
+
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Employee employee)
+        {
+            var createdEmployee =
+                await _employeeService.CreateEmployeeAsync(employee);
+
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id = createdEmployee.EmployeeId },
+                createdEmployee);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Employee employee)
+        {
+            if (id != employee.EmployeeId)
+                return BadRequest();
+
+            var updated =
+                await _employeeService.UpdateEmployeeAsync(employee);
+
+            if (!updated)
+                return NotFound();
+
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleted =
+                await _employeeService.DeleteEmployeeAsync(id);
+
+            if (!deleted)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
