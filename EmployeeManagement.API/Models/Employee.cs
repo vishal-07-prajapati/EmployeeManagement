527096bb2cc34cc9a6d6744c0c47a35f0f2cb107
vishal-07-prajapati <vishalpprajaparti1234@outlook.com>
2026-07-09 11:56:26 +0530

    Add Employee entity model

---
+++ EmployeeManagement.API/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManagement.API.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string Department { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(150)]
+        public string Email { get; set; } = string.Empty;
+
+        [Range(0, 1000000)]
+        public decimal Salary { get; set; }
+
+        public DateTime JoiningDate { get; set; } = DateTime.UtcNow;
+
+        public bool IsActive { get; set; } = true;
+    }
+}
