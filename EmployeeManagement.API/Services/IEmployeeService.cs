60f3f5c1cf4150126dbb5789046e6c2d68060486
vishal-07-prajapati <vishalpprajaparti1234@outlook.com>
2026-07-09 13:46:02 +0530

    Add employee service interface

---
+++ EmployeeManagement.API/Services/IEmployeeService.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagement.API.Models;
+
+namespace EmployeeManagement.API.Services
+{
+    public interface IEmployeeService
+    {
+        Task<IEnumerable<Employee>> GetAllEmployeesAsync();
+
+        Task<Employee?> GetEmployeeByIdAsync(int id);
+
+        Task<Employee> CreateEmployeeAsync(Employee employee);
+
+        Task<bool> UpdateEmployeeAsync(Employee employee);
+
+        Task<bool> DeleteEmployeeAsync(int id);
+    }
+}
